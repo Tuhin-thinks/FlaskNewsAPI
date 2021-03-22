42e7d5577c91e04b7260e85a05679f867ab86abe
tuhin-thinks <tuhinmitra190221@gmail.com>
2021-03-22 23:15:18 +0530

    added home page;updated style

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, redirect, url_for
 import get_latest_news as news
 
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def home():
+def index():
+    return redirect(url_for('home'))
+
+@app.route('/apidata')
+def json_data(page=1):
     data = news.getNews()
     return jsonify({"data":data})
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
